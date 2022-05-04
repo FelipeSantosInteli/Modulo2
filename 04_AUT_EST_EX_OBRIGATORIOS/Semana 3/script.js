5ac972f02a6ed1fb04fca42b5eed09182bff5a38
FelipeSantosInteli <felipe.santos@sou.inteli.edu.br>
2022-05-04 14:48:58 -0300

    Exercícios semana 3

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector("#send");
+
+function calcMaxH(v) {
+    console.log("Valor recebido de v0: "+v)
+    var h = Math.pow(v, 2)/19.6
+    return(h)
+};
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const v0 = Number(document.querySelector("#value").value);
+    const MaxH = calcMaxH(v0);
+    
+    document.getElementById("output").innerHTML = parseFloat(MaxH).toFixed(2) + "m";
+    console.log(MaxH);
+    console.log(v0);
+
+});
+
+
+
+
