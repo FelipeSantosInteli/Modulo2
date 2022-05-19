e15be5ac5e405b2d56afc3c605264417d5dec6ab
FelipeSantosInteli <felipe.santos@sou.inteli.edu.br>
2022-05-19 11:06:06 -0300

    etapa 5

--- 03_AUT_EST_ENTREGA/Semana 4/script.js
+++ 03_AUT_EST_ENTREGA/Semana 4/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    var btn = $('.btn');
+    btn.on('click', function(e) {
+        $('html, body').animate({
+            scrollTop: 0
+        });
+        e.preventDefault();
+    })
+});
+
 window.addEventListener("scroll", function(){
     var img = document.querySelector("img");
     img.classList.toggle("sticky", window.scrollY > 0)
@@ -11,4 +21,4 @@ window.addEventListener("scroll", function(){
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("stickyNAV", window.scrollY > 0)  
-})  
+})
